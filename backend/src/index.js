9ff12591298406edb4b99b0113fd03aee55e812e
Yasho321 <yashovardhans321@gmail.com>
2025-05-29 02:43:48 +0530

    Managed Limiter

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 5000, // limit each IP to 100 requests per windowMs
   message: 'Too many requests, please try again later.'
 });
 
